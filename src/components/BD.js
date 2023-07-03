a2bbad44e93df876c352eb713f47e9fad3914a74
Alissonn1325 <alissonfbrito01@Gmail.com>
2023-07-03 11:53:23 -0300

    bala

---
+++ src/components/BD.js
@@ -0,0 +1,454 @@
+const Arrayperguntas = [
+  {
+    id: 1,
+    pergunta: 'Qual jogador foi eleito o melhor do mundo em 2021 pela FIFA',
+    imagem: 0,
+    respostas: [
+      {
+        id: 1.1,
+        questao1: 'Cristiano Ronaldo',
+        resposta: false
+      },
+
+      {
+        id: 1.2,
+        questao2: 'Lionel Messi',
+        resposta: true
+      },
+
+      {
+        id: 1.3,
+        questao3: 'Robert Lewandowski',
+        resposta: false
+      },
+
+      {
+        id: 1.4,
+        questao4: 'Kylian Mbappe',
+        resposta: false
+      }
+    ]
+  },
+
+  {
+    id: 2,
+    pergunta: 'Qual é o único jogador a ter marcado em duas finais de Copa do Mundo diferentes?',
+    imagem: 0,
+    respostas: [
+      {
+        id: 2.1,
+        questao1: 'Péle',
+        resposta: true
+      },
+
+      {
+        id: 2.2,
+        questao2: 'Diego Maradona',
+        resposta: false
+      },
+
+      {
+        id: 2.3,
+        questao3: 'Zinedine Zidane',
+        resposta: false
+      },
+
+      {
+        id: 2.4,
+        questao4: 'Ronaldo Fenômeno',
+        resposta: false
+      }
+    ]
+  },
+  {
+    id: 3,
+    pergunta: 'Qual jogador é o artilheiro máximo da história da Copa do Mundo?',
+    imagem: 0,
+    respostas: [
+      {
+        id: 3.1,
+        questao1: 'Ronaldo Fenômeno',
+        resposta: false
+      },
+
+      {
+        id: 3.2,
+        questao2: 'Lionel Messi',
+        resposta: false
+      },
+
+      {
+        id: 3.3,
+        questao3: 'Miroslav Klose',
+        resposta: true
+      },
+
+      {
+        id: 3.4,
+        questao4: 'Péle',
+        resposta: false
+      }
+    ]
+  },
+  {
+    id: 4,
+    pergunta: 'Qual jogador marcou o gol que deu à seleção brasileira o título da Copa do Mundo de 1994?',
+    imagem: 0,
+    respostas: [
+      {
+        id: 4.1,
+        questao1: 'Romário',
+        resposta: true
+      },
+
+      {
+        id: 4.2,
+        questao2: 'Bebeto',
+        resposta: false
+      },
+
+      {
+        id: 4.3,
+        questao3: 'Branco',
+        resposta: false
+      },
+
+      {
+        id: 4.4,
+        questao4: 'Dunga',
+        resposta: false
+      }
+    ]
+  },
+  {
+    id: 5,
+    pergunta: 'Qual o jogador com mais títulos na história do futebol?',
+    imagem: 0,
+    respostas: [
+      {
+        id: 5.1,
+        questao1: 'Péle',
+        resposta: false
+      },
+
+      {
+        id: 5.2,
+        questao2: 'Daniel Alves',
+        resposta: true
+      },
+
+      {
+        id: 5.3,
+        questao3: 'Cafu',
+        resposta: false
+      },
+
+      {
+        id: 5.4,
+        questao4: 'Benzema',
+        resposta: false
+      }
+    ]
+  },
+  {
+    id: 6,
+    pergunta: 'Qual jogador é o maior artilheiro da história do Campeonato Brasileiro?',
+    imagem: 0,
+    respostas: [
+      {
+        id: 6.1,
+        questao1: 'Zico',
+        resposta: false
+      },
+
+      {
+        id: 6.2,
+        questao2: 'Romário',
+        resposta: false
+      },
+
+      {
+        id: 6.3,
+        questao3: 'Péle',
+        resposta: false
+      },
+
+      {
+        id: 6.4,
+        questao4: 'Roberto Dinamite',
+        resposta: true
+      }
+    ]
+  },
+  {
+    id: 7,
+    pergunta: 'Qual é o jogador com mais gols marcados em uma única edição da Liga dos Campeões da UEFA?',
+    imagem: 0,
+    respostas: [
+      {
+        id: 7.1,
+        questao1: 'Lionel Messi',
+        resposta: false
+      },
+
+      {
+        id: 7.2,
+        questao2: 'Cristiano Ronaldo',
+        resposta: true
+      },
+
+      {
+        id: 7.3,
+        questao3: 'Gerd Müller',
+        resposta: false
+      },
+
+      {
+        id: 7.4,
+        questao4: 'Ferenc Puskás',
+        resposta: false
+      }
+    ]
+  },
+  {
+    id: 8,
+    pergunta: 'Qual jogador foi eleito o melhor do mundo pela primeira vez em 1991?',
+    imagem: 0,
+    respostas: [
+      {
+        id: 8.1,
+        questao1: 'Ronaldo Fenômeno',
+        resposta: false
+      },
+
+      {
+        id: 8.2,
+        questao2: 'Zinedine Zidane',
+        resposta: false
+      },
+
+      {
+        id: 8.3,
+        questao3: 'Péle',
+        resposta: false
+      },
+
+      {
+        id: 8.4,
+        questao4: 'Lothar Matthäus',
+        resposta: true
+      }
+    ]
+  },
+  {
+    id: 9,
+    pergunta: 'Qual país eliminou o Brasil na copa do mundo de 2014?',
+    imagem: 0,
+    respostas: [
+      {
+        id: 9.1,
+        questao1: 'França',
+        resposta: false
+      },
+
+      {
+        id: 9.2,
+        questao2: 'Argentina',
+        resposta: false
+      },
+
+      {
+        id: 9.3,
+        questao3: 'Croácia',
+        resposta: false
+      },
+
+      {
+        id: 9.4,
+        questao4: 'Alemanha',
+        resposta: true
+      }
+    ]
+  },
+  {
+    id: 10,
+    pergunta: 'Em que ano a seleção brasileira conquistou sua primeira Copa do Mundo?',
+    imagem: 0,
+    respostas: [
+      {
+        id: 10.1,
+        questao1: '1958',
+        resposta: true
+      },
+
+      {
+        id: 10.2,
+        questao2: '1962',
+        resposta: false
+      },
+
+      {
+        id: 10.3,
+        questao3: '1966',
+        resposta: false
+      },
+
+      {
+        id: 10.4,
+        questao4: '1970',
+        resposta: false
+      }
+    ]
+  },
+  {
+    id: 11.1,
+    pergunta: 'Qual é o único país a ter conquistado a Copa do Mundo em três continentes diferentes?',
+    imagem: 0,
+    respostas: [
+      {
+        id: 11.1,
+        questao1: 'Brasil',
+        resposta: true
+      },
+
+      {
+        id: 11.2,
+        questao2: 'Alemanha',
+        resposta: false
+      },
+
+      {
+        id: 11.3,
+        questao3: 'Argentina',
+        resposta: false
+      },
+
+      {
+        id: 11.4,
+        questao4: 'Itália',
+        resposta: false
+      }
+    ]
+  },
+  {
+    id: 12,
+    pergunta: 'Em que ano a França conquistou sua primeira Copa do Mundo?',
+    imagem: 0,
+    respostas: [
+      {
+        id: 12.1,
+        questao1: '1998',
+        resposta: true
+      },
+
+      {
+        id: 12.2,
+        questao2: '2002',
+        resposta: false
+      },
+
+      {
+        id: 12.3,
+        questao3: '2006',
+        resposta: false
+      },
+
+      {
+        id: 12.4,
+        questao4: '2010',
+        resposta: false
+      }
+    ]
+  },
+  {
+    id: 13,
+    pergunta: 'Qual é o clube com mais títulos da Liga dos Campeões da UEFA?',
+    imagem: 0,
+    respostas: [
+      {
+        id: 13.1,
+        questao1: 'Barcelona',
+        resposta: false
+      },
+
+      {
+        id: 13.2,
+        questao2: 'Bayern de Munique',
+        resposta: false
+      },
+
+      {
+        id: 13.3,
+        questao3: 'Real Madrid',
+        resposta: true
+      },
+
+      {
+        id: 13.4,
+        questao4: 'Manchester United',
+        resposta: false
+      }
+    ]
+  },
+  {
+    id: 14,
+    pergunta: 'Qual é o clube com mais títulos do Campeonato Espanhol?',
+    imagem: 0,
+    respostas: [
+      {
+        id: 14.1,
+        questao1: 'Real Madrid',
+        resposta: true
+      },
+
+      {
+        id: 14.2,
+        questao2: 'Barcelona',
+        resposta: false
+      },
+
+      {
+        id: 14.3,
+        questao3: 'Atlético de Madrid',
+        resposta: false
+      },
+
+      {
+        id: 14.4,
+        questao4: 'Valencia',
+        resposta: false
+      }
+    ]
+  },
+  {
+    id: 15,
+    pergunta: 'Qual clube conquistou mais títulos da Copa Libertadores da América?',
+    imagem: 0,
+    respostas: [
+      {
+        id: 15.1,
+        questao1: 'Boca Juniors',
+        resposta: false
+      },
+
+      {
+        id: 15.2,
+        questao2: 'River Plate',
+        resposta: false
+      },
+
+      {
+        id: 15.3,
+        questao3: 'Independiente',
+        resposta: true
+      },
+
+      {
+        id: 15.4,
+        questao4: 'Peñarol',
+        resposta: false
+      }
+    ]
+  },
+]
+export { Arrayperguntas }
--- src/components/BD.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-questao = { pergunta = "Qual jogador foi eleito o melhor do mundo em 2021 pela FIFA"
-{
-"Cristiano Ronaldo"
-"Lionel Messi"
-"Robert Lewandowski"
-"Kylian Mbappe"
-}}
-</script>
